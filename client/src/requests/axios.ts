9dbfe1120b2f2575611ef92aa92dae3dd938b011
Johann Monteiro <monteiro.johann@gmail.com>
2023-03-14 21:29:24 -0300

    change on axios.ts

--- client/src/requests/axios.ts
+++ client/src/requests/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://172.19.48.1:3333/'
+  // baseURL: 'http://172.19.48.1:3333/'
+  baseURL: 'http://192.168.15.11:3333/'
 })
 
 export type ApiResponse = {
